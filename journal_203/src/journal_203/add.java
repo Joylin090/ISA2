fed91f66247f8d0b0bc7e8df8810a386da0bac74
dcunh <dcunh@DESKTOP-2H0VOVT.mshome.net>
2024-03-21 18:50:56 +0530

    commitone

---
+++ journal_203/src/journal_203/add.java
@@ -0,0 +1,17 @@
+package journal_203;
+
+public class add {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Joylin D'Cunha RollNo: 203
+		
+			    int first = 5;
+			    int second = 4;
+
+			    // add two numbers
+			    int sum = first + second;
+			    System.out.println(first + " + " + second + " = "  + sum);
+			  }
+
+}
---
+++ journal_203/src/journal_203/addtest.java
@@ -0,0 +1,27 @@
+package journal_203;
+
+import static org.junit.Assert.assertEquals;
+
+
+import org.junit.Test;
+
+public class addtest {
+	 @Test
+	    public void testAddition() {
+	        int first = 5;
+	        int second = 4;
+	        int expectedResult = 9;
+	        int actualResult = first + second;
+	        assertEquals(expectedResult, actualResult);
+	    }
+
+	    @Test
+	    public void testNegativeNumbers() {
+	        int first = -2;
+	        int second = -7;
+	        int expectedResult = -9;
+	        int actualResult = first + second;
+	        assertEquals(expectedResult, actualResult);
+	    }
+}
+	
